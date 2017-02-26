80f43d915f907eb0af3ed9321b10dba2980128cf
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2017-02-25 22:11:38 -0500

    Implement install command

---
+++ src/NuGet.Manage/NuGetHelper.cs
@@ -0,0 +1,72 @@
+﻿using NuGet.Protocol.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using NuGet.Configuration;
+using NuGet.Frameworks;
+using System.Threading;
+using NuGet.Protocol;
+
+namespace NuGet.Manage
+{
+    public class NuGetHelper
+    {
+        public class PackageInformation
+        {
+            public SourcePackageDependencyInfo SourcePackageDependencyInfo { get; }
+            public SourceRepository SourceRepository { get; }
+
+            public PackageInformation(SourcePackageDependencyInfo sourcePackageDependencyInfo, SourceRepository sourceRepository) {
+                SourcePackageDependencyInfo = sourcePackageDependencyInfo;
+                SourceRepository = sourceRepository;
+            }
+        }
+
+        public static void PrepareSourceRepositoriesFrom(List<SourceRepository> sourceRepositories, string configFile) {
+            PackageSourceProvider p = new PackageSourceProvider(new Settings(Path.GetDirectoryName(configFile), Path.GetFileName(configFile), true));
+            var sources = p.LoadPackageSources();
+
+            foreach (var source in sources) {
+                sourceRepositories.Add(new SourceRepository(source, Repository.Provider.GetCoreV3()));
+            }
+        }
+
+        public static async Task<PackageInformation> DetermineNewestPackage(List<SourceRepository> sourceRepositories, string packageId) {
+            SourcePackageDependencyInfo latestPackage = null;
+            SourceRepository selectedSourceRepository = null;
+
+            foreach (var sourceRepository in sourceRepositories) {
+                if (sourceRepository.PackageSource?.Credentials?.Password != null) {
+                    await sourceRepository.GetResourceAsync<HttpHandlerResource>();
+                }
+                var resource = await sourceRepository.GetResourceAsync<DependencyInfoResource>();
+                if (resource != null) {
+                    var results = await resource.ResolvePackages(packageId, NuGetFramework.AnyFramework, new Common.NullLogger(), CancellationToken.None);
+
+                    if (results.Count() > 0) {
+                        foreach (var result in results) {
+                            if (latestPackage == null) {
+                                latestPackage = result;
+                                selectedSourceRepository = sourceRepository;
+                            } else {
+                                if (result.Version.CompareTo(latestPackage.Version) > 0) {
+                                    latestPackage = result;
+                                    selectedSourceRepository = sourceRepository;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if (latestPackage == null && selectedSourceRepository == null) {
+                return null;
+            } else {
+                return new PackageInformation(latestPackage, selectedSourceRepository);
+            }
+        }
+    }
+}
--- src/NuGet.Manage/Program.cs
+++ src/NuGet.Manage/Program.cs
@@ -1,14 +1,70 @@
-﻿using System;
+﻿using Microsoft.Framework.Runtime.Common.CommandLine;
+using NuGet.Protocol.Core.Types;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace NuGet.Manage
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static int Main(string[] args) {
+            var app = new CommandLineApplication();
+
+            app.Command("install", c =>
+            {
+                var configFileOption = c.Option("-c|--configfile", "Configuration file to use", CommandOptionType.SingleValue);
+                var packageArgument = c.Argument("package", "Package to install");
+
+                c.OnExecute(async () =>
+                {
+                    if (!configFileOption.HasValue()) {
+                        Console.Error.WriteLine("No configuration file specified. Unable to continue.");
+                        return 1;
+                    }
+
+                    if (string.IsNullOrEmpty(packageArgument.Value)) {
+                        Console.Error.WriteLine("No package specified. Unable to continue.");
+                        return 1;
+                    }
+
+                    var sourceRepositories = new List<SourceRepository>();
+                    NuGetHelper.PrepareSourceRepositoriesFrom(sourceRepositories, configFileOption.Value());
+
+                    var package = await NuGetHelper.DetermineNewestPackage(sourceRepositories, packageArgument.Value);
+
+                    HttpClientHandler handler;
+                    if (package.SourceRepository.PackageSource.Credentials != null) {
+                        handler = new HttpClientHandler()
+                        {
+                            Credentials = new NetworkCredential(package.SourceRepository.PackageSource.Credentials.Username, package.SourceRepository.PackageSource.Credentials.Password)
+                        };
+                    } else {
+                        handler = new HttpClientHandler();
+                    }
+
+                    using (handler) {
+                        using (var httpClient = new HttpClient(handler)) {
+                            using (var stream = await httpClient.GetStreamAsync(package.SourcePackageDependencyInfo.DownloadUri)) {
+                                using (var zipArchive = new ZipArchive(stream)) {
+                                    var targetDirectory = Path.Combine(Directory.GetCurrentDirectory(), $"{package.SourcePackageDependencyInfo.Id}.{package.SourcePackageDependencyInfo.Version.ToFullString()}");
+
+                                    zipArchive.ExtractToDirectory(targetDirectory);
+                                }
+                            }
+                        }
+                    }
+
+                    return 0;
+                });
+            });
+
+            return app.Execute(args);
         }
     }
 }
